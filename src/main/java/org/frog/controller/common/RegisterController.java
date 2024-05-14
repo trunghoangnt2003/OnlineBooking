6731fe26bac5ad81797c01199569482a380c3e3b
trunghoangnt2003 <120396322+trunghoangnt2003@users.noreply.github.com>
2024-05-14 11:38:16 +0700

    login/register

--- src/main/java/org/frog/controller/common/RegisterController.java
+++ src/main/java/org/frog/controller/common/RegisterController.java
@@ -3,9 +3,13 @@ package org.frog.controller.common;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
+import org.frog.DAO.AccountDAO;
+import org.frog.model.Account;
 import org.frog.utility.Email;
 
 import java.io.IOException;
+import java.util.UUID;
+
 @WebServlet("/register")
 public class RegisterController extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res){
@@ -15,15 +19,19 @@ public class RegisterController extends HttpServlet {
             throw new RuntimeException(e);
         }
     }
-    public void doPost(HttpServletRequest req, HttpServletResponse res) {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String userName = req.getParameter("userName");
+        String passWord = req.getParameter("passWord");
         String email = req.getParameter("email");
-        Email sendEmail = new Email();
-        sendEmail.sendEmail("Test","test user "+email,email);
-        try {
-            req.getRequestDispatcher("view/public/login.jsp").forward(req,res);
-        } catch (ServletException | IOException e) {
-            throw new RuntimeException(e);
-        }
+        String phone = req.getParameter("phone");
+        UUID uuid = UUID.randomUUID();
+        String id = uuid.toString();
+        System.out.println(id);
+        Account account = new Account(id,null,userName,passWord,null,phone,email,null);
+        AccountDAO accountDAO = new AccountDAO();
+        accountDAO.save(account);
+        req.getRequestDispatcher("view/public/login.jsp").forward(req,res);
+
     }
 
 }
