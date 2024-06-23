e68ea98ec667430eec7b4e2e06017adcd7418174
trunghoangnt2003 <120396322+trunghoangnt2003@users.noreply.github.com>
2024-06-24 00:12:56 +0700

    test

--- src/main/java/org/frog/DAO/WalletDAO.java
+++ src/main/java/org/frog/DAO/WalletDAO.java
@@ -1,10 +1,9 @@
 package org.frog.DAO;
 
-import org.frog.model.Account;
-import org.frog.model.Mentee;
-import org.frog.model.Wallet;
+import org.frog.model.*;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class WalletDAO {
     public Wallet getByAccountId(String id) {
@@ -75,13 +74,145 @@ public class WalletDAO {
 
     }
 
-    public Account getWalletAccountById(String id) {
-        return null;
+    public void updateAvailable(Wallet wallet, float available){
+        try{
+
+            String sql = "UPDATE [dbo].[Wallet]\n" +
+                    "   SET [available] = ?\n" +
+                    " WHERE id = ?";
+            Connection connection = JDBC.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setFloat(1, available);
+            preparedStatement.setInt(2, wallet.getId());
+
+            preparedStatement.executeUpdate();
+            JDBC.closeConnection(connection);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public Account getWalletAccountById(String account_id){
+        Account account = new Account();
+        String sql = "SELECT w.id,balance,available,a.id AS account_id,name,role_id FROM Wallet w \n" +
+                "JOIN Account a ON w.id = a.wallet_id\n" +
+                "where a.id = ?";
+        try{
+            PreparedStatement stm = JDBC.getConnection().prepareStatement(sql);
+            stm.setString(1,account_id);
+            ResultSet resultSet = stm.executeQuery();
+            if(resultSet.next()){
+                account.setId(resultSet.getString("account_id"));
+                account.setName(resultSet.getString("name"));
+
+                Wallet wallet = new Wallet();
+                wallet.setId(resultSet.getInt("id"));
+                wallet.setBalance(resultSet.getFloat("balance"));
+                wallet.setAvailable(resultSet.getFloat("available"));
+                account.setWallet(wallet);
+
+                Role role = new Role();
+                role.setId(resultSet.getInt("role_id"));
+                account.setRole(role);
+                return account;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return account;
+    }
+    public void createTransaction(Timestamp date , float amount,int walletFrom , int walletTo , float fee ){
+        String sql = "INSERT INTO [dbo].[Transaction]\n" +
+                "           ([date]\n" +
+                "           ,[amount]\n" +
+                "           ,[type_id]\n" +
+                "           ,[wallet_id]\n" +
+                "           ,[wallet_opposite]\n" +
+                "           ,[fee])          \n" +
+                "     VALUES\n" +
+                "           (?\n" +
+                "           ,?\n" +
+                "           ,3\n" +
+                "           ,?\n" +
+                "           ,?\n" +
+                "           ,?)";
+        try{
+            PreparedStatement stm = JDBC.getConnection().prepareStatement(sql);
+            stm.setTimestamp(1,date);
+            stm.setFloat(2,amount);
+            stm.setInt(3,walletFrom);
+            stm.setInt(4,walletTo);
+            stm.setFloat(5,fee);
+            stm.executeUpdate();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public boolean payment(float amount,int wallet_id){
+        String sql ="UPDATE [Wallet]\n" +
+                "   SET [balance] = ?   \n" +
+                " WHERE id = ?";
+        try{
+            PreparedStatement stm = JDBC.getConnection().prepareStatement(sql);
+            stm.setFloat(1,amount);
+            stm.setInt(2,wallet_id);
+            int i = stm.executeUpdate();
+            if(i>0){
+                return true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public boolean moneyBack(float amount,int wallet_id){
+        String sql ="UPDATE [Wallet]\n" +
+                "   SET [available] = ?   \n" +
+                " WHERE id = ?";
+        try{
+            PreparedStatement stm = JDBC.getConnection().prepareStatement(sql);
+            stm.setFloat(1,amount);
+            stm.setInt(2,wallet_id);
+            int i = stm.executeUpdate();
+            if(i>0){
+                return true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
     }
 
-    public void payment(float v, int id) {
-    }
+    public ArrayList<Transaction> getAllTransactionByWalletId(int id) {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        try {
+            Connection connection = JDBC.getConnection();
+            String sql = "select A.name, T.id, T.amount, T.fee, TT.name as type_name, T.date\n" +
+                    "                    from Account A join Wallet W on A.wallet_id = W.id\n" +
+                    "                    join [Transaction] T on W.id = T.wallet_opposite\n" +
+                    "                    join Type_Transaction TT on TT.id = T.type_id\n" +
+                    "\t\t\t\t\tjoin Status S on S.id = T.status_id\n" +
+                    "                    where T.wallet_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Transaction transaction = new Transaction();
+                TypeTransaction typeTransaction = new TypeTransaction();
+                Account account = new Account();
+                transaction.setId(resultSet.getInt("id"));
+                transaction.setDate(resultSet.getDate("date"));
+                transaction.setAmount(resultSet.getFloat("amount"));
+                transaction.setFee(resultSet.getFloat("fee"));
+                typeTransaction.setName(resultSet.getString("type_name"));
+                account.setName(resultSet.getString("name"));
+                transaction.setTypeTransaction(typeTransaction);
+                transaction.setAccount(account);
+                transactions.add(transaction);
 
-    public void createTransaction(Timestamp timestamp, int amount, int id, int id1, float fee) {
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return transactions;
     }
 }
